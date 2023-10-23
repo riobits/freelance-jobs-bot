1efcb0e1ededbca985cf53c2b15b527390152142
riobits <23579928+riobits@users.noreply.github.com>
2023-10-23 14:52:44 +0300

    Add command to get channel id

--- src/bot.ts
+++ src/bot.ts
@@ -27,6 +27,14 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.launch()
 
+bot.on('channel_post', (ctx) => {
+  const postContent = (ctx.channelPost as any).text
+
+  if (postContent === '/id') {
+    ctx.sendMessage(`${ctx.chat.id}`)
+  }
+})
+
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
